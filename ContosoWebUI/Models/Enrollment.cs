1aa094f0a2a682f347c1cefeb4eed8d35f0644cc
GitProjectJockey <ewnordin@comcast.net>
2017-01-04 13:40:49 -0700

    Added Complex Data Model

--- ContosoWebUI/Models/Enrollment.cs
+++ ContosoWebUI/Models/Enrollment.cs
@@ -10,10 +10,17 @@ namespace ContosoWebUI.Models
     {
         [Key]
         public int EnrollmentID { get; set; }
+
         public int CourseID { get; set; }
+
         public int StudentID { get; set; }
+
+        [DisplayFormat(NullDisplayText = "No grade")]
         public Grade? Grade { get; set; }
+
         public virtual Course Course { get; set; }
+
         public virtual Student Student { get; set; }
+
     }
 }
